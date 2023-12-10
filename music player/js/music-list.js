966edf9171c45d4561ed3ab3b66080473ab1df19
RexShark <131393823+RexShark@users.noreply.github.com>
2023-12-10 13:24:08 +0530

    Add files via upload

---
+++ music player/js/music-list.js
@@ -0,0 +1,63 @@
+let allMusic= [
+    {
+        name: "On My Way",
+        artist: "Alan Walker",
+        img: "01.jpg",
+        src:"music1.mp3",
+    },
+    {
+        name: "Lovely",
+        artist: "Billie Ellish",
+        img: "02.jpg",
+        src:"music2.mp3",
+    },
+    {
+        name: "We Dont Talk Anymore",
+        artist: "Charlie Puth",
+        img: "03.jpg",
+        src:"music3.mp3",
+    },
+    {
+        name: "Photograph",
+        artist: "Ed Shareen",
+        img: "05.jpg",
+        src:"music5.mp3",
+    },
+    {
+        name: "Turn Back time",
+        artist: "Daniel Socheluz",
+        img: "06.jpg",
+        src:"music6.mp3",
+    },
+    {
+        name: "Senorita",
+        artist: "Shawn Mendes",
+        img: "09.jpg",
+        src:"music9.mp3",
+    },
+    {
+        name: "Falling",
+        artist: "Trevor Daniel",
+        img: "07.jpg",
+        src:"music7.mp3",
+    },
+    {
+        name: "Hawayien",
+        artist: "Arijith Singh",
+        img: "11.jpg",
+        src:"music11.mp3",
+    },
+    {
+        name: "Raatan Lambiyan",
+        artist: "Aseess Kaur",
+        img: "13.jpg",
+        src:"music13.mp3",
+    },
+    {
+        name: "Dil Diyan Gallan",
+        artist: "Atif Aslam",
+        img: "17.jpg",
+        src:"music17.mp3",
+    }
+
+]
